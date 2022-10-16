b5943a6503d397872250b50504d175c6333f35fb
AdrianFred <78622520+AdrianFred@users.noreply.github.com>
2022-10-16 23:41:24 +0200

    Update profile.js

--- javascript/api/actions/profile.js
+++ javascript/api/actions/profile.js
@@ -16,35 +16,4 @@ document.getElementById("logout").addEventListener("click", () => {
   window.location.href = "../pages/login.html";
 });
 
-// const myPosts = async () => {
-//   const response = await fetch(url, {
-//     method: "GET",
-//     headers: {
-//       "Content-Type": "application/json",
-//       Authorization: `Bearer ${token}`,
-//     },
-//   });
-
-//   const test = response.json();
-//   test.then((data) => {
-//     data.forEach((post) => {
-//       if (post.author.name == names) {
-//         console.log(post);
-//         document.getElementById("recents").innerHTML += `
-
-//         <form action="../pages/specific.html" method="GET">
-//             <div class="col-xl-6 offset-xl-3 mt-4 bg-dark rounded p-2">
-//                 <div class=""><h3>@${post.author.name}</h3></div>
-//                 <div class="">
-//                     <p> ${post.body}</p>
-//                 </div>
-//                 <input type="hidden" name="id" value="${post.id}">
-//                 <input type="submit" value="View Post" class="btn btn-primary">
-//             </div>
-//         </form>`;
-//       }
-//     });
-//   });
-// };
-
 myPosts();
